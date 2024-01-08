e08af9265d73ddbd79a4166b11cbd33bfcfa32dd
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-01-08 20:59:29 -0300

    Altera model de produto #2

--- src/main/java/com/mar/back/model/Produto.java
+++ src/main/java/com/mar/back/model/Produto.java
@@ -1,9 +1,13 @@
 package com.mar.back.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,4 +24,9 @@ public class Produto {
     private String nome;
 
     private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    @JsonIgnoreProperties("produtos")
+    private Categoria categoria;
 }
