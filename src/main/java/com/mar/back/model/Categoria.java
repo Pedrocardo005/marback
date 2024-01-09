cc4eeb7222311fe80c087c771f65c92ffb746cb9
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-01-08 21:01:08 -0300

    Add model de categoria #3

---
+++ src/main/java/com/mar/back/model/Categoria.java
@@ -0,0 +1,34 @@
+package com.mar.back.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    @JsonIgnoreProperties("categoria")
+    private List<Produto> produtos;
+
+    public Categoria(Long id) {
+        this.id = id;
+    }
+}
